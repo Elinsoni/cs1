f8e02d9911b2242f1c6dbce78b31b77912b9ca0a
Elinsoni <elinson536@gmail.com>
2018-12-01 12:58:07 +0300

    2 заданниеб пытаюсь решить через тру парсб пока не выходит

--- Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Program.cs
@@ -55,6 +55,49 @@ namespace Lesson3
             var pr = z * d; //демонстрация работы оператора умножения класса комплекс
             Console.WriteLine(pr);
             Console.ReadLine();
+
+
+            #region
+            /// <summary>
+            /// Задание 2 С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.Сами числа и сумму вывести на экран исп TryParse
+            /// </summary>
+            /// <param name="n"></param>
+             void Summa(int number)
+            {
+                int sum = 0;
+                do //зажаем цикл который будет работать пока введенное число с клавиатуры будет 0
+                {
+
+                    if (number > 0 && number % 2 != 0) // если введенное число положительное и нечетное (при делении на 2 с отбрасыванием целой части получем не 0)
+                        sum = sum + number;
+                    Console.WriteLine("Введите число: "); // затем запрашиваем следущее число для ввода с клавиатуры для которого наш цыкл повторится
+                    number = Convert.ToInt32(Console.ReadLine());
+                    /*WriteandCheck();*/
+                } while (number != 0);
+                Console.WriteLine("Сумма нечетных, положительных чисел = {0}", sum); //вывод результата сложения, подходящих под условие введенных цифр
+                Console.ReadLine();
+            }
+            #endregion
+
+           /* void WriteandCheck()
+            {
+
+                Console.WriteLine("Введите число: "); // запрашиваем ввод числа с клавиатуры
+                string usernumber;
+                usernumber =Int32.TryParse(Console.ReadLine()); // кладем запрошенное число в переменную
+                if (usernumber==0)
+                {
+                    throw new DivideByZeroException("Введены не коректные данные ");
+                }
+                
+            }*/
+            Console.WriteLine("Введите число: "); // запрашиваем ввод числа с клавиатуры
+
+            int usernumber = Convert.ToInt32(Console.ReadLine()); // кладем запрошенное число в переменну
+
+            Summa(usernumber); // запускаем метод сложения положительных, нечетных чисел
+
+
         }
     }
 }
