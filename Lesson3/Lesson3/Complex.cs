4a8dfe5e19f465a9423c7a92575ae80370a44e7b
Elinsoni <elinson536@gmail.com>
2018-12-01 11:22:47 +0300

    Добавила структуру комплексного числа и в классе комплексного числа добавила операторы + и -

--- Lesson3/Lesson3/Complex.cs
+++ Lesson3/Lesson3/Complex.cs
@@ -11,10 +11,11 @@ namespace Lesson3
         private double _Re; //орпеделяем поле объекта комлексное чило - вещественную часть
         private double _Im; // определяем поле объекта комлексное число - мнимую часть (по треб-м инкапсуляции делаем их приват)
 
-        public Сomplex ()  //создаем конструктор "по умлочанию"
+        /*public Сomplex ()  //создаем конструктор "по умлочанию"
         {
 
         }
+        */
         public Complex(double Re, double Im)  //конструктор который принимает в ка-ве параметров 2 числа
         {
             _Re = Re;
@@ -37,6 +38,26 @@ namespace Lesson3
         public void SetIm (double value) //процедура установки Im
         {
             _Im = value;
+        }
+
+        public double Abs     //gлучение абсолютного значения числа
+        {
+            get
+            {
+                return Math.Sqrt(_Re * _Re + _Im * _Im);
+            }
+        }
+        public static Complex operator +(Complex a, Complex b) // определяем оператор сложения "+"
+        {
+            return new Complex(a._Re + b._Re, a._Im + b._Im);
+        }
+         public static Complex operator -(Complex a, Complex b) //определяем оператор вычитания "-" ЗАДАНИЕ 1 б
+        {
+            return new Complex(a._Re - b._Re, a._Im - b._Im);
+        }
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a._Re * b._Re, a._Im * b._Im);
         }
     }
 }
--- Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace Lesson3
 {
+    struct Complex  // описываем структуру
+    {
+        public double im;
+        public double re;
+        public Complex Plus (Complex x)
+        {
+            Complex y;
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        } 
+        public Complex Minus (Complex x) //добавляем 
+        {
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Complex z = new Complex(10, 15);
+            var re = z.GetRe();
+            z.SetIm(100);
+            Console.WriteLine(z);
+            Console.ReadLine();
         }
     }
 }
